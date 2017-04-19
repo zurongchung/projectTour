bba529547ed0282ed034c2b5ae843c20fe75f641
zurongchung <uzurongchung@gmail.com>
2017-04-19 22:11:43 +0800

    --

--- app/products.rb
+++ app/products.rb
@@ -3,12 +3,14 @@ require_relative 'core'
 ROOT = '../'
 _IN = ROOT + 'haml/app.html.haml'
 _OUT = ROOT + 'release/html/app.html'
-CONFIG = ROOT + 'config/combo.json'
+COMBO_INFO = ROOT + 'config/combo.json'
+NOTE_INFO = ROOT + 'config/note.json'
 class Product < HTML
   attr_accessor :combo
   def initialize(_i, _o)
     super(_i, _o)
-    @combo = JSON.parse(readConfig(CONFIG))
+    @combo = JSON.parse(readConfig(COMBO_INFO))
+    @notes = JSON.parse(readConfig(NOTE_INFO))
   end
   def readConfig(_f)
     if(File.exist?(_f))
