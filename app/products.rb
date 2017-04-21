dff4fb5d7a9f6c90a3e244985a3506377a6aee98
zurongchung <uzurongchung@gmail.com>
2017-04-21 18:05:29 +0800

    add self tour page

--- app/products.rb
+++ app/products.rb
@@ -5,12 +5,15 @@ _IN = ROOT + 'haml/app.html.haml'
 _OUT = ROOT + 'release/html/app.html'
 COMBO_INFO = ROOT + 'config/combo.json'
 NOTE_INFO = ROOT + 'config/note.json'
+SELF_CONTENT_DATA = ROOT + 'config/self_tour.json'
 class Product < HTML
   attr_accessor :combo
   def initialize(_i, _o)
     super(_i, _o)
     @combo = JSON.parse(readConfig(COMBO_INFO))
     @notes = JSON.parse(readConfig(NOTE_INFO))
+    @selfTour = ROOT + 'haml/free_tour.html.haml'
+    @self_tour_data = JSON.parse(readConfig(SELF_CONTENT_DATA))
   end
   def readConfig(_f)
     if(File.exist?(_f))
