401bb5e055c57cceb1c9f47c75898c6f9f557fa4
zuRong Chung <zurongchung@gmail.com>
2017-04-23 05:34:34 -0700

    almost finish

--- app/home.rb
+++ app/home.rb
@@ -5,11 +5,19 @@ _IN = ROOT + 'haml/app.html.haml'
 _OUT = ROOT + 'release/html/app.html'
 DB_HOME = ROOT + 'db/home.json'
 class Home < HTML
-  attr_accessor :data
+  attr_accessor :data, :h2hDataExchange
   def initialize(_i, _o)
     super(_i, _o)
     @home = ROOT + 'haml/home.html.haml'
+    @homeIntroInfo = ROOT + 'haml/home/intro-text-info.html.haml'
     @data = JSON.parse(readData(DB_HOME))
+    @h2hDataExchange = nil
+  end
+  def setData(data)
+    @h2hDataExchange = data
+  end
+  def getData
+    @h2hDataExchange
   end
 end
 home = Home.new(_IN, _OUT)
