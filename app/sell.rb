cde8a9143fc5ee10da8e68ac245d1a6a65b778c0
zurongchung <zurongchung@gmail.com>
2017-04-24 14:32:49 +0800

    quick nav

--- app/sell.rb
+++ app/sell.rb
@@ -4,12 +4,15 @@ ROOT = '../'
 _IN = ROOT + 'haml/sell.html.haml'
 _OUT = ROOT + 'release/html/sell.html'
 DB_SELL = ROOT + 'db/sell/sell.json'
+DB_QUICK_NAV = ROOT + 'db/sell/quickNav.json'
 class Sell < HTML
   attr_accessor :sellDataDB
   def initialize(_i, _o)
     super(_i, _o)
     @sellContents = ROOT + 'haml/sell/sell-contents.html.haml'
+    @quickNavContents = ROOT + 'haml/sell/quick-nav.html.haml'
     @sellDB = JSON.parse(readData(DB_SELL))
+    @quickNav = JSON.parse(readData(DB_QUICK_NAV))
   end
 end
 sell = Sell.new(_IN, _OUT)
