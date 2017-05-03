a93bbcd55a5246de02e399a250e766e0c6ab2908
zurongchung <zurongchung@gmail.com>
2017-05-03 15:11:39 +0800

    extract graphics

--- app/sell.rb
+++ app/sell.rb
@@ -7,6 +7,7 @@ DB_SELL = ROOT + 'db/sell/sell.json'
 DB_QUICK_NAV = ROOT + 'db/sell/quickNav.json'
 DB_CAROUSEL = ROOT + 'db/sell/verList.json'
 DB_SCENES = ROOT + 'db/sell/scenePreview.json'
+DB_GRAPHICS = ROOT + 'db/sell/graphics.json'
 class Sell < HTML
   attr_accessor :sellDataDB
   def initialize(_i, _o)
@@ -15,6 +16,7 @@ class Sell < HTML
     @quickNavContents = ROOT + 'haml/sell/quick-nav.html.haml'
     #@carouselElement = ROOT + "haml/sell/carouselA.html.haml"
     @carouselElement = ROOT + "haml/sell/scene-intro.html.haml"
+    @graphics = JSON.parse(readData(DB_GRAPHICS))
     @sellDB = JSON.parse(readData(DB_SELL))
     @scenes = JSON.parse(readData(DB_SCENES))
     @quickNav = JSON.parse(readData(DB_QUICK_NAV))
