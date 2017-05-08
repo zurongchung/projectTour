93916219a554e61d98460bbdf96180107ea21dc9
zurongchung <zurongchung@gmail.com>
2017-05-08 12:17:15 +0800

    --

--- app/products.rb
+++ app/products.rb
@@ -3,17 +3,14 @@ require_relative 'core'
 ROOT = '../'
 _IN = ROOT + 'haml/product.html.haml'
 _OUT = ROOT + 'release/html/product.html'
-DB_COMBO = ROOT + 'db/combo.json'
-DB_NOTE = ROOT + 'db/note.json'
-DB_SELF_CONTENT_DATA = ROOT + 'config/self_tour.json'
+DB_PAGE_A = ROOT + 'db/page/pageA.json'
+DB_PAGE_QUICK_LINKS = ROOT + 'db/page/quickLinks.json'
 class Product < HTML
-  attr_accessor :combo
   def initialize(_i, _o)
     super(_i, _o)
-    @combo = JSON.parse(readData(DB_COMBO))
-    @notes = JSON.parse(readData(DB_NOTE))
-    @selfTour = ROOT + 'haml/free_tour.html.haml'
-    @self_tour_data = JSON.parse(readData(DB_SELF_CONTENT_DATA))
+    @selfTour = ROOT + 'haml/page/pageA.html.haml'
+    @selfTourData = JSON.parse(readData(DB_PAGE_A))
+    @selfTourLinksData = JSON.parse(readData(DB_PAGE_QUICK_LINKS))
   end
 end
 p = Product.new(_IN, _OUT)
